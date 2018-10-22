b466b2e84cf6ea5831664f29a56f0f319ab66933
talbert1209 <talbert1209@gmail.com>
2018-10-22 13:48:37 -0500

    Add project files.

---
+++ InterfacePractice/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InterfacePractice
+{
+    class Program
+    {
+        public class TallGuy
+        {
+            public string Name;
+            public int Height;
+
+            public void TalkAboutYourself()
+            {
+                Console.WriteLine($"My name is {Name} and I'm {Height} inches tall");
+            }
+        }
+        static void Main(string[] args)
+        {
+            TallGuy tallGuy = new TallGuy() { Height = 74, Name = "Jimmy" };
+            tallGuy.TalkAboutYourself();
+        }
+    }
+}
